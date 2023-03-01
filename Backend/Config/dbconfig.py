bb4c330b54c6d479e037408a61e222f4cab84b07
MISAEL MONTES <misael.montes@upr.edu>
2023-03-01 18:26:57 -0400

    Added dbconfig.py

---
+++ Backend/Config/dbconfig.py
@@ -0,0 +1,10 @@
+#Databse configuration information
+
+dbconfig = {
+    'user' : 'uisfwadbozfrpe',
+    'password' : '9dc2bc185141ba6460ebae7237da058090128de81ddeec62ef0e5ea88ec9519b',
+    'dbname' : 'dbur7kaf8mpa86',
+    'dbhost' : 'ec2-3-220-207-90.compute-1.amazonaws.com',
+    'dbport': 5432
+
+}
